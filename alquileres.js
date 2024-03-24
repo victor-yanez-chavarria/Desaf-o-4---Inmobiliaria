269dcb664cbe027f87e08ed77dea771226491b66
Victor.Yanez <dev.victor.chavarria@gmail.com>
2024-03-24 16:27:32 -0300

    first commit

---
+++ alquileres.js
@@ -0,0 +1,74 @@
+const alquileres = [
+  {
+    nombre: 'Apartamento en el centro de la ciudad',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion:
+      'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+    ubicacion: '123 Main Street, Anytown, CA 91234',
+    habitaciones: 2,
+    banos: 2,
+    costo: 2000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: 'Apartamento luminoso con vista al mar',
+    src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    descripcion:
+      'Este hermoso apartamento ofrece una vista impresionante al mar',
+    ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+    habitaciones: 3,
+    banos: 3,
+    costo: 2700,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: 'Condominio moderno en zona residencial',
+    src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+    descripcion:
+      'Este elegante condominio moderno está ubicado en una zona residencial',
+    ubicacion: '123 Main Street, Anytown, CA 91234',
+    habitaciones: 2,
+    banos: 2,
+    costo: 2200,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: 'Apartamento afueras de gran manzana',
+    src: 'https://img.jamesedition.com/listing_images/2024/02/19/14/01/06/d8b19ee4-8ad2-47e4-ad29-a7d5f3070620/je/760x470xc.jpg',
+    descripcion:
+      'Vivienda proporciona a los residentes acceso conveniente a servicios.',
+    ubicacion: '123 Main Street, Anytown, CA 91223',
+    habitaciones: 2,
+    banos: 2,
+    costo: 2400,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: 'Casa de lujo',
+    src: 'https://img.jamesedition.com/listing_images/2022/10/13/09/10/29/0fa87801-2ff0-4caf-a797-d246d4e48b7e/je/760x470xc.jpg',
+    descripcion:
+      'Espectacular nido de pájaro de 1955 a mediados de siglo con total privacidad y amplias vistas',
+    ubicacion: 'California, Estados Unidos 90068',
+    habitaciones: 4,
+    banos: 4,
+    costo: 3000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: 'Casa de lujo',
+    src: 'https://http2.mlstatic.com/D_NQ_NP_628221-MLC73914150884_012024-O.webp',
+    descripcion:
+      'Town House Ciudad Parque de Chicauma Chile, emplazado en un entorno familiar',
+    ubicacion: 'Chicauma Ciudad Parque Lampa',
+    habitaciones: 4,
+    banos: 4,
+    costo: 1000,
+    smoke: true,
+    pets: true,
+  },
+];
---
+++ venta.js
@@ -0,0 +1,74 @@
+const ventas = [
+  {
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion:
+      'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    banos: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: 'Apartamento acogedor en la montaña',
+    src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+    descripcion:
+      'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+    ubicacion: '89 Mountain Road, Summit Peaks, CA 23456',
+    habitaciones: 2,
+    banos: 1,
+    costo: 1200,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: 'Penthouse de lujo con terraza panorámica',
+    src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+    descripcion:
+      'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+    ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+    habitaciones: 3,
+    banos: 3,
+    costo: 4500,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: 'Penthouse de lujo en zona exclusiva',
+    src: 'https://st2.depositphotos.com/1041088/11595/i/450/depositphotos_115954550-stock-photo-home-exterior-with-garage-and.jpg',
+    descripcion:
+      'Construida para proporcionar refugio, comodidad y seguridad a sus habitantes',
+    ubicacion: '1234 Sunset Boulevard, Los Angeles, CA 90028',
+    habitaciones: 4,
+    banos: 4,
+    costo: 12000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: 'Penthouse California',
+    src: 'https://us.123rf.com/450wm/iriana88w/iriana88w1402/iriana88w140200260/25668327-fachada-de-la-casa-bonita-con-porche-colomn-y-garaje-anexo-el-c%C3%A9sped-verde-con-cama-de-flores-y.jpg?ver=6',
+    descripcion:
+      'Consta de varias habitaciones diseñadas para diferentes funciones',
+    ubicacion: '456 Oak Street, Los Angeles, CA 90001',
+    habitaciones: 3,
+    banos: 3,
+    costo: 10000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: 'Apartamento Los Angeles',
+    src: 'https://wise.com/imaginary-v2/f95c2c9d0c417e30dbff81c0f1a94860.jpg',
+    descripcion:
+      'Espacio habitable diseñado de manera compacta y eficiente',
+    ubicacion: '456 Oak Street, Los Angeles, CA 90001',
+    habitaciones: 2,
+    banos: 2,
+    costo: 7000,
+    smoke: false,
+    pets: false,
+  },
+];
